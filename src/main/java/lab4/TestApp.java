d8db7b76c3fabf177acffffb77c6525bed30e3b8
belikova-julia <alistami38@gmail.com>
2021-12-17 23:06:52 +0300

    Scripted auto-commit on change (2021-12-17 23:06:52) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -1,4 +1,12 @@
 package lab4;
 
+import akka.actor.ActorSystem;
+
 public class TestApp {
+    private static final String SYSTEM_NAME = "testing";
+
+    public static void main(String[] args) throws Exception {
+        ActorSystem system = ActorSystem.create(SYSTEM_NAME);
+        
+    }
 }
