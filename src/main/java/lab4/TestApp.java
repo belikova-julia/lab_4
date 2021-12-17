7570ff90b11df4ddb46984dbe5f642ef024000ed
belikova-julia <alistami38@gmail.com>
2021-12-18 00:40:32 +0300

    Scripted auto-commit on change (2021-12-18 00:40:32) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -9,7 +9,6 @@ import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.routing.RouterActor;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
