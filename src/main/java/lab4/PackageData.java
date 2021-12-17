402e83065060f775e70cb2f61a03a2908894b9d8
belikova-julia <alistami38@gmail.com>
2021-12-17 23:59:11 +0300

    Scripted auto-commit on change (2021-12-17 23:59:11) by gitwatch.sh

--- src/main/java/lab4/PackageData.java
+++ src/main/java/lab4/PackageData.java
@@ -1,6 +1,7 @@
 package lab4;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class PackageData {
