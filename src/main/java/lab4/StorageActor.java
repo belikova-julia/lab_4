89f64f3c8ea321461e30ec4f19d77bb781751e09
belikova-julia <alistami38@gmail.com>
2021-12-18 01:24:21 +0300

    Scripted auto-commit on change (2021-12-18 01:24:21) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -10,15 +10,29 @@ import java.util.Map;
 public class StorageActor extends AbstractActor {
     private final Map<String, ArrayList<String>> storage = new HashMap<>();
 
-    private void sendResult(GetMessage msg) {
-        sender().tell();
+    private void sendResults(GetMessage msg) {
+        sender().tell(
+                new AllResultsMessage(msg.getPackageId(), storage.get(msg.getPackageId())),
+                getContext().parent()
+        );
+    }
+
+    private void save(ResultMessage msg) {
+        ArrayList<String> results = storage.get(msg.getPackageId());
+        if (results != null) {
+            results.add(msg.getResult());
+        } else {
+            results = new ArrayList<>();
+            results.add(msg.getResult());
+            storage.put(msg.getPackageId(), results);
+        }
     }
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(GetMessage.class, )
-                .match(ResultMessage.class, )
+                .match(GetMessage.class, this::sendResults)
+                .match(ResultMessage.class, this::save)
                 .build();
     }
 }
