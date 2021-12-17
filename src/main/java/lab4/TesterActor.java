374b63a98e8f62d6202fd1aa036e2463cd533b8d
belikova-julia <alistami38@gmail.com>
2021-12-18 01:30:17 +0300

    Scripted auto-commit on change (2021-12-18 01:30:17) by gitwatch.sh

--- src/main/java/lab4/TesterActor.java
+++ src/main/java/lab4/TesterActor.java
@@ -1,10 +1,13 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TesterActor extends AbstractActor {
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match()
+                .build();
     }
 }
