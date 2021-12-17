9c7ad39fbb17910310479ec75ea07ddc6422440b
belikova-julia <alistami38@gmail.com>
2021-12-18 00:25:41 +0300

    Scripted auto-commit on change (2021-12-18 00:25:41) by gitwatch.sh

--- src/main/java/lab4/TestData.java
+++ src/main/java/lab4/TestData.java
@@ -5,11 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 public class TestData {
+    @JsonProperty("testName")
     private final String testName;
+    @JsonProperty("expectedResult")
     private final String expectedResult;
+    @JsonProperty("params")
     private final ArrayList<Object> params;
 
-    public TestData() {
-
+    public TestData(@JsonProperty("testName") String testName,
+                    @JsonProperty("expectedResult") String expectedResult,
+                    @JsonProperty("params") ArrayList<Object> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
     }
 }
