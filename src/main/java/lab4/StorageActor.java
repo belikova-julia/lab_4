a80bd55bb3a62009b56caf6a6b0f72ff32e32819
belikova-julia <alistami38@gmail.com>
2021-12-18 00:44:18 +0300

    Scripted auto-commit on change (2021-12-18 00:44:18) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -1,4 +1,10 @@
 package lab4;
 
-public class StorageActor {
+import akka.actor.AbstractActor;
+
+public class StorageActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
