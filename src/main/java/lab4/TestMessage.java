9a57ba2aaade928ed004fd4014fb14c152eabb89
belikova-julia <alistami38@gmail.com>
2021-12-18 01:36:01 +0300

    Scripted auto-commit on change (2021-12-18 01:36:01) by gitwatch.sh

--- src/main/java/lab4/TestMessage.java
+++ src/main/java/lab4/TestMessage.java
@@ -1,4 +1,38 @@
 package lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class TestMessage {
+    private final String packageId, jsScript, functionName;
+    private final ArrayList<TestData> tests;
+
+    @JsonCreator
+    public PackageData(@JsonProperty("packageId") String packageId,
+                       @JsonProperty("jsScript") String jsScript,
+                       @JsonProperty("functionName") String functionName,
+                       @JsonProperty("tests") ArrayList<TestData> tests) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public ArrayList<TestData> getTests() {
+        return tests;
+    }
 }
