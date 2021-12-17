fdea07fe8079488403d18b7fa673f76a79113e61
belikova-julia <alistami38@gmail.com>
2021-12-18 01:08:19 +0300

    Scripted auto-commit on change (2021-12-18 01:08:19) by gitwatch.sh

--- src/main/java/lab4/ResultMessage.java
+++ src/main/java/lab4/ResultMessage.java
@@ -1,4 +1,19 @@
 package lab4;
 
 public class ResultMessage {
+    private final String packageId;
+    private final String result;
+
+    public ResultMessage(String packageId, String result) {
+        this.packageId = packageId;
+        this.result = result;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
 }
