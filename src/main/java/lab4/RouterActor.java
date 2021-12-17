8393171461a274dfde369da62ed6c5015ebed50c
belikova-julia <alistami38@gmail.com>
2021-12-18 02:40:38 +0300

    Scripted auto-commit on change (2021-12-18 02:40:38) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -1,23 +1,31 @@
 package lab4;
 
-import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
-import akka.actor.Props;
+import akka.actor.*;
+import akka.japi.pf.DeciderBuilder;
 import akka.japi.pf.ReceiveBuilder;
 import akka.routing.ActorRefRoutee;
 import akka.routing.RandomRoutingLogic;
 import akka.routing.Routee;
 import akka.routing.Router;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+import static akka.actor.SupervisorStrategy.restart;
+
 public class RouterActor extends AbstractActor {
-    private Router router;
-    private ActorRef storage;
+    private final Router router;
+    private final ActorRef storage;
 
     private static final String STORAGE_NAME = "storage";
     private static final int NUM_WORKERS = 5;
+    private static final int MAX_RETRIES = 5;
+    private static final Duration DURATION = Duration.ofMinutes(1);
+
+    private static SupervisorStrategy strategy = new OneForOneStrategy(
+            MAX_RETRIES, DURATION,
+            DeciderBuilder.matchAny(o -> restart()).build())
 
     public RouterActor() {
         storage = getContext().actorOf(Props.create(StorageActor.class), STORAGE_NAME);
