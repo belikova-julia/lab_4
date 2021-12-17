3a60445f36bed30e414feb12a430e00cba8ca589
belikova-julia <alistami38@gmail.com>
2021-12-17 23:18:35 +0300

    Scripted auto-commit on change (2021-12-17 23:18:35) by gitwatch.sh

--- src/main/java/lab4/HttpParse.java
+++ src/main/java/lab4/HttpParse.java
@@ -1,4 +1,16 @@
 package lab4;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.server.Route;
+
 public class HttpParse {
+    private final ActorRef router;
+
+    public HttpParse(ActorRef routerActor) {
+        this.router = routerActor;
+    }
+
+    public Route createRoute() {
+        return route()
+    }
 }
