4c9bdb2517b9129cb058dcd07b6d68bd96bca4b3
belikova-julia <alistami38@gmail.com>
2021-12-17 23:12:10 +0300

    Scripted auto-commit on change (2021-12-17 23:12:10) by gitwatch.sh

--- src/main/java/lab4/TestApp.java
+++ src/main/java/lab4/TestApp.java
@@ -2,12 +2,19 @@ package lab4;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.routing.RouterActor;
+import akka.stream.ActorMaterializer;
 
 public class TestApp {
     private static final String SYSTEM_NAME = "testing";
 
     public static void main(String[] args) throws Exception {
         ActorSystem system = ActorSystem.create(SYSTEM_NAME);
-        ActorRef 
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class), "router");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        
     }
 }
