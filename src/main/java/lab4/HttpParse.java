e422589a3dd528ac41ead42a6401f0ea43a46574
belikova-julia <alistami38@gmail.com>
2021-12-17 23:30:13 +0300

    Scripted auto-commit on change (2021-12-17 23:30:13) by gitwatch.sh

--- src/main/java/lab4/HttpParse.java
+++ src/main/java/lab4/HttpParse.java
@@ -1,7 +1,11 @@
 package lab4;
 
 import akka.actor.ActorRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import scala.concurrent.Future;
+import static akka.http.javadsl.server.Directives.*;
 
 public class HttpParse {
     private final ActorRef router;
@@ -13,8 +17,10 @@ public class HttpParse {
     public Route createRoute() {
         return route(
                 get(() -> parameter("packageID", (ID) -> {
-                    Future
-                }))
+                    Future<Object> future = Patterns.ask(router, );
+                    return completeOKWithFuture(future, Jackson.marshaller());
+                })),
+                post(() -> entity(Jackson.u))
         );
     }
 }
