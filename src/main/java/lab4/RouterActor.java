561896313588c2669ffc4c20c6478eb2ef83e271
belikova-julia <alistami38@gmail.com>
2021-12-18 02:08:44 +0300

    Scripted auto-commit on change (2021-12-18 02:08:44) by gitwatch.sh

--- src/main/java/lab4/RouterActor.java
+++ src/main/java/lab4/RouterActor.java
@@ -4,7 +4,7 @@ import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
 public class RouterActor extends AbstractActor {
-    
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
