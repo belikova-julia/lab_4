bfb8c1c8430fb20b73400c6b732487f7b59f41a2
belikova-julia <alistami38@gmail.com>
2021-12-18 01:13:35 +0300

    Scripted auto-commit on change (2021-12-18 01:13:35) by gitwatch.sh

--- src/main/java/lab4/StorageActor.java
+++ src/main/java/lab4/StorageActor.java
@@ -1,10 +1,24 @@
 package lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageActor extends AbstractActor {
+    private final Map<String, ArrayList<String>> storage = new HashMap<>();
+
+    private void sendResult(GetMessage msg) {
+        sender().tell();
+    }
+
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(GetMessage.class, )
+                .match(ResultMessage.class, )
+                .build();
     }
 }
