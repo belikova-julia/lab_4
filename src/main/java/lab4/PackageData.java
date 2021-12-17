e1d3bbfd6ff578b5e9d394dbec49a12d40a22b1f
belikova-julia <alistami38@gmail.com>
2021-12-17 23:58:38 +0300

    Scripted auto-commit on change (2021-12-17 23:58:38) by gitwatch.sh

--- src/main/java/lab4/PackageData.java
+++ src/main/java/lab4/PackageData.java
@@ -1,6 +1,12 @@
 package lab4;
 
+import java.util.ArrayList;
+
 public class PackageData {
     private final String packageId;
-    private final 
+    private final String jsScript;
+    private final String functionName;
+    private final ArrayList<> tests;
+
+    
 }
